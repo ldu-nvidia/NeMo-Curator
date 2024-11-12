82f0211cb8b63b34e3f17ca97c672ffaf944f4ad
ldu-nvidia <ldu@nvidia.com>
2024-11-12 12:09:15 -0800

    updated storing hf access token locally

--- tutorials/supervised_fine_tuning/code/setup_bare_metal.sh
+++ tutorials/supervised_fine_tuning/code/setup_bare_metal.sh
@@ -15,25 +15,24 @@
 
 # please read the README file first for prerequisites
 
-python3 -m venv venv
-source venv/bin/activate
-
+#python3 -m venv venv
+#source venv/bin/activate
+# read hf access token from token.env
+source token.env
 echo "installing dependency needed for data curation"
-pip install -r code/requirements.txt
-pip install cython
-pip install --extra-index-url https://pypi.nvidia.com ".[cuda12x]"
+#pip install -r code/requirements.txt
+#pip install cython
+#pip install --extra-index-url https://pypi.nvidia.com ".[cuda12x]"
 
 # install huggingface cli
 echo "installing huggingface hub"
 pip3 install -U "huggingface_hub[cli]"
 
-# put your own hf token here, read from files saved securely
-#HF_TOKEN= ""
 # shell script to log into huggingface-hub with token
 echo "login to huggingface cli"
-huggingface-cli login --token $HF_TOKEN --add-to-git-credential
+huggingface-cli login --token $HF_ACCESS_TOKEN --add-to-git-credential
 # might need his to update the datasets package
-pip3 install -U datasets
+#pip3 install -U datasets
 
 # create directory for storing model and download model from hf
 mkdir mistral-7B-hf
@@ -42,7 +41,7 @@ huggingface-cli download mistralai/Mistral-7B-v0.1 --local-dir mistral-7B-hf
 echo "downloading model checkpoint, this will take a while..."
 
 # eiter way should work: pulling and run a prebuilt container or install NeMo framework from the source
-echo "pull and run nemo training container"
-docker run --gpus device=1 --shm-size=2g --net=host --ulimit memlock=-1 --rm -it -v ${PWD}:/workspace -w /workspace -v ${PWD}/results:/results nvcr.io/nvidia/nemo:24.07 bash
+#echo "pull and run nemo training container"
+#docker run --gpus device=1 --shm-size=2g --net=host --ulimit memlock=-1 --rm -it -v ${PWD}:/workspace -w /workspace -v ${PWD}/results:/results nvcr.io/nvidia/nemo:24.07 bash
 
 
