0232cf8d789782402eaea278fa42d0b5feed4768
ldu <ldu@nvidia.com>
2024-11-12 21:06:48 -0800

    modified installation script due to package updates

--- tutorials/supervised_fine_tuning/code/setup_bare_metal.sh
+++ tutorials/supervised_fine_tuning/code/setup_bare_metal.sh
@@ -17,8 +17,8 @@
 
 # create virtual environment for dependency isolation
 cd ~/
-python3 -m venv sft
-source sft/bin/activate
+python3 -m venv test
+source test/bin/activate
 
 # install NeMo framework: from source
 git clone https://github.com/NVIDIA/NeMo.git
@@ -37,23 +37,68 @@ pip3 install -r requirements.txt
 source token.env
 echo "installing huggingface hub"
 pip3 install -U "huggingface_hub[cli]==0.23.2"
-pip3 install -U datasets
+pip3 install -U datasets==2.1.0
 
 # shell script to log into huggingface-hub with token
 echo "login to huggingface cli"
 huggingface-cli login --token $HF_ACCESS_TOKEN --add-to-git-credential
 
 # clone the nemo-curator repo
+echo "pulling git curator repo"
+git clone https://github.com/ldu-nvidia/NeMo-Curator.git
+echo "installing dependency for NeMo-Curator"
+pip3 install cython
+pip3 install --extra-index-url https://pypi.nvidia.com "./NeMo-Curator[all]"
 
+cd NeMo-Curator/tutorials/supervised_fine_tuning/code/
+echo "install packages needed for SFT playbook"
+pip3 install -r requirements.txt
 
 # create directory for storing model and download model from hf
+cd ..
 mkdir mistral-7B-hf
 echo "downloading mistral model checkpoint into folder"
 huggingface-cli download mistralai/Mistral-7B-v0.1 --local-dir mistral-7B-hf
 echo "downloading model checkpoint, this will take a while..."
 
+echo "convert nemo model from .hf format to .nemo format, this will take a while..."
+# TODO have issue converting
+pip3 install transformers --upgrade
+pip3 install torch 
+python3 ~/NeMo/scripts/checkpoint_converters/convert_mistral_7b_hf_to_nemo.py --input_name_or_path=./mistral-7B-hf/ --output_path=mistral.nemo
+echo "model format conversion finished!"
+
+# might need this for data curation to work
+#pip3 install -U datasets
 # eiter way should work: pulling and run a prebuilt container or install NeMo framework from the source
 #echo "pull and run nemo training container"
 #docker run --gpus device=1 --shm-size=2g --net=host --ulimit memlock=-1 --rm -it -v ${PWD}:/workspace -w /workspace -v ${PWD}/results:/results nvcr.io/nvidia/nemo:24.07 bash
 
 
+
+
+
+python3 data_curation.py
+
+"""# verify the size and integrity of the file
+du -sh databricks-dolly-15k/databricks-dolly-15k.jsonl;
+sha256sum databricks-dolly-15k/databricks-dolly-15k.jsonl
+
+echo "preprocess data sources to follow correct format"
+
+# could be skipped if the format is correct already
+python3 /opt/NeMo-Framework-Launcher/launcher_scripts/nemo_launcher/collections/dataprep_scripts/dolly_dataprep/preprocess.py --input databricks-dolly-15k/databricks-dolly-15k.jsonl
+
+# sanity check for the downloaded data
+echo "checking if jsonl files exist!"
+ls databricks-dolly-15k/
+echo "check first three examples in the output jsonl file!"
+head -n 3 databricks-dolly-15k/databricks-dolly-15k-output.jsonl
+
+# generate data and sanity check
+echo "generating train validation test dataset"
+
+echo "check train val test data are generated"
+cd databricks-dolly-15k/
+echo "check train, val, test data are generated"
+ls"""
